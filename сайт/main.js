64cb217c2aca0367f09f537d89e4db5f22f426ff
o <evilduck777@gmail.com>
2023-01-07 23:35:09 +0300

    Added Ice Cream Dessert and Vegetarian salad

--- сайт/main.js
+++ сайт/main.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', () => {
 
 let search = document.querySelector('.search-box');
 
- document.querySelector('#search-icon').onclick = () => {
+document.querySelector('#search-icon').onclick = () => {
 	search.classList.toggle('active');
 	menu.classList.remove('active')
- }
+}
