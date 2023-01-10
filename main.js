223eaa5ff2aa547c7c5e2e4af1a4c89d56eac59e
ranizine <yaroslav76071@mail.ru>
2023-01-10 20:09:25 +0300

    updated html

--- main.js
+++ main.js
@@ -4,19 +4,11 @@ let navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
 	header.classList.toggle('active', window.scrollY > 0);
-	
-}) 
-
-let search = document.querySelector('.search-box');
-
-document.querySelector('#search-icon').onclick = () => {
-	search.classList.toggle('active');
-	menu.classList.remove('active')
-}
+});
 
 menu.onclick = () => { 
-	navbar.classList.toggle('active')
+	navbar.classList.toggle('active');
 }
 window.onscroll = () => {
-	navbar.classList.remove('active')
+	navbar.classList.remove('active');
 }
