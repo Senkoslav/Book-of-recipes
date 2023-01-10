69c40b2add728f15d9758a85be82f8069a10bc0c
ranizine <yaroslav76071@mail.ru>
2023-01-10 20:00:24 +0300

    updated adaptiv

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let header = document.querySelector('header');
+let menu = document.querySelector('menu-icon');
+let navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
 	header.classList.toggle('active', window.scrollY > 0);
@@ -10,4 +12,11 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
 	search.classList.toggle('active');
 	menu.classList.remove('active')
+}
+
+menu.onclick = () => { 
+	navbar.classList.toggle('active')
+}
+window.onscroll = () => {
+	navbar.classList.remove('active')
 }
