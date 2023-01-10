41e7faced9358ccb4964333341c8734af39b94ed
ranizine <yaroslav76071@mail.ru>
2023-01-10 20:59:15 +0300

    commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let header = document.querySelector('header');
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let search = document.querySelector('.search-box');
 
 window.addEventListener('scroll', () => {
 	header.classList.toggle('active', window.scrollY > 0);
@@ -11,4 +12,10 @@ menu.onclick = () => {
 }
 window.onscroll = () => {
 	navbar.classList.remove('active')
+}
+
+//поиск
+document.querySelector('#search-icon').onclick = () => {
+  search.classList.toggle('active');
+  menu.classList.remove('active')
 }
